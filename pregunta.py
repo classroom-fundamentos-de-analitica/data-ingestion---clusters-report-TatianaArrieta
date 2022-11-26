e783ccb56b86fbb63b17a186a3f2eaaada02be11
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-26 09:38:52 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,53 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    contador = 0
+    dictionary = {}
+    dataframe = pd.DataFrame()
+
+    with open('clusters_report.txt') as clustersreport:
+        rows = clustersreport.readlines()
+    rows = rows[4:]
+    clusters = []
+    cluster = [0, 0, 0, '']
+    lastCluster = 1
+    lastPClave = 105
+    lastPorcentaje = "15,9 %"
+    for i, _ in df.iterrows():
+        if df.iloc[i,0] != lastCluster and not pd.isna(df.iloc[i,0]):
+            lastCluster = df.iloc[i,0]
+            lastPClave = df.iloc[i,1]
+            lastPorcentaje = df.iloc[i,2]
+        else:
+            df.iloc[i,0] = lastCluster
+            df.iloc[i,1] = lastPClave
+            df.iloc[i,2] = lastPorcentaje
+
+        for line in clustersreport:
+            line = re.sub(r"\s+", " ", line)
+            if len(line)>1 and contador > 3:
+                if line.split()[0].isnumeric() == True:
+                    try: 
+                        dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+                        df = df.append(dictionary, ignore_index=True)
+                    except: pass
+                    dictionary = {'cluster': int(line.split()[0]),
+                                'cantidad_de_palabras_clave': int(line.split()[1]),
+                                'porcentaje_de_palabras_clave': float(line.split()[2].replace(',','.')),
+                                'principales_palabras_clave': line.split()[4:]}
+                else: 
+                    dictionary['principales_palabras_clave'].append(' '.join(line.split()))
+                    
+            contador += 1
+            
+    dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+    df = df.append(dictionary, ignore_index=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.rstrip('\.')
+    
 
     return df
